6bed08b1dab3c69914e4f38c1c32ed4b9b5a6bba
yh.cho <youngho7482@gmail.com>
2021-12-06 18:01:10 +0900

    Login Form Developing

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import AuthForm from '../components/auth/AuthForm';
+import AuthTemplate from '../components/auth/AuthTemplate';
 
 
 const LoginPage = () => {
-  return <div>로그인</div>;
+  return(
+    <AuthTemplate>
+      <AuthForm/>
+    </AuthTemplate>
+  );
 };
 
 export default LoginPage;
