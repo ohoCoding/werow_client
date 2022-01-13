4c2ac4f68ab02339c4d90e6f8947fbdb1a0c7ed9
youngho <youngho7482@gmail.com>
2022-01-14 03:30:02 +0900

    page develop

---
+++ src/pages/Login.tsx
@@ -0,0 +1,249 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
+import { actionCreators as userActions } from '../redux/modules/user';
+
+// 로그인 페이지
+function Login(props: { history: any }) {
+    const { history } = props;
+    const dispatch = useDispatch();
+    const [userId, SetUserId] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const login = () => {
+        if (userId === '' || password === '') {
+            window.alert('로그인 정보를 모두 입력해주세요.');
+            return;
+        }
+        dispatch(userActions.LoginDB(userId, password));
+    };
+    return (
+        <Container>
+            <Header>
+                <HeaderImg src="" alt="WEROW" />
+            </Header>
+            <MainContainer>
+                <TitleBox>
+                    <Title>통합ID 로그인</Title>
+                </TitleBox>
+                {/* <LoginTitleBox>
+                    <LoginTitle>통합ID 로그인</LoginTitle>
+                </LoginTitleBox> */}
+
+                <InputBox>
+                    <Input
+                        type="text"
+                        placeholder="이메일"
+                        onChange={(e) => {
+                            SetUserId(e.target.value);
+                        }}
+                    />
+                </InputBox>
+                <InputBox>
+                    <Input
+                        type="password"
+                        placeholder="비밀번호"
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                    />
+                </InputBox>
+                <FindId>
+                    <span>
+                        이메일/비밀번호 찾기{' '}
+                        <img src="https://auth.dano.me/res/images/ec1e39bf2b6a3857a9e6bd2c3364a67f.png" alt="화살표" />
+                    </span>
+                </FindId>
+                <div>
+                    <LoginBtn onClick={login}>로그인</LoginBtn>
+                    <SignupBtn
+                        onClick={() => {
+                            history.push('/user/signup');
+                        }}
+                    >
+                        회원가입
+                    </SignupBtn>
+                </div>
+                <SnsBox>
+                    <SnsText>SNS로 시작하기</SnsText>
+                    <SnsBtnBox>
+                        <KakaoBtn
+                            onClick={() => {
+                                history.push('/prepare');
+                            }}
+                        >
+                            <img
+                                src="https://auth.dano.me/res/images/49c343639ceea64b1fe7f46e2d6442ef.svg"
+                                alt="카톡"
+                            />
+                            카카오톡
+                        </KakaoBtn>
+
+                        <AppleBtn
+                            onClick={() => {
+                                history.push('/prepare');
+                            }}
+                        >
+                            <img
+                                src="https://blog.kakaocdn.net/dn/czMTX6/btqNbvGUwIu/xxqSeZd4eRMvTHqbfIZUd0/img.png"
+                                alt="네이버"
+                            />
+                            NAVER
+                        </AppleBtn>
+                    </SnsBtnBox>
+                </SnsBox>
+            </MainContainer>
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    margin: auto;
+    min-width: 290px;
+    max-width: 460px;
+    position: relative;
+`;
+
+const Header = styled.div`
+    height: 42px;
+    line-height: 42px;
+    background: #fff;
+    text-align: center;
+    font-size: 15px;
+    font-weight: bold;
+    color: #0d0d0d;
+    border-bottom: 1px solid #ebebeb;
+`;
+
+const HeaderImg = styled.img`
+    font-size: 18px;
+    height: 16px;
+`;
+const MainContainer = styled.div`
+    margin: auto 40px;
+`;
+
+const TitleBox = styled.div`
+    margin-top: 55px;
+`;
+const Title = styled.div`
+    font-size: 21px;
+    font-weight: bold;
+    text-align: center;
+    color: #0d0d0d;
+    margin-bottom: 5px;
+`;
+const LoginTitleBox = styled.div`
+    margin-top: 30px;
+`;
+const LoginTitle = styled.div`
+    color: #ff6f61;
+    font-weight: bold;
+    text-align: center;
+    padding-bottom: 10px;
+    border-bottom: 2px solid #ff6f61;
+`;
+const InputBox = styled.div`
+    margin-top: 15px;
+`;
+
+const Input = styled.input`
+    width: 100%;
+    border: 1px solid #e7e7e7;
+    background-color: #fff;
+    text-align: left;
+    font-size: 13px;
+    color: #2c2c2c;
+    padding: 0 15px;
+    height: 45px;
+    box-sizing: border-box;
+`;
+
+const FindId = styled.div`
+    text-align: right;
+    margin: 12px 0;
+    font-size: 13px;
+    & span {
+        color: #b0b0b0;
+        text-decoration: underline;
+        margin: 0;
+        padding: 0;
+        cursor: pointer;
+        & img {
+            width: 7px;
+            margin-left: 5px;
+        }
+    }
+`;
+const LoginBtn = styled.button`
+    width: 100%;
+    height: 45px;
+    border-radius: 2px;
+    font-size: 14px;
+    border: 1px solid #e7e7e7;
+    background-color: #fbfbfb;
+    color: #ff6f61;
+    cursor: pointer;
+`;
+const SignupBtn = styled.button`
+    margin-top: 10px;
+    color: #fff;
+    background-color: #ff6f61;
+    width: 100%;
+    height: 45px;
+    border-radius: 2px;
+    font-size: 14px;
+    border: 1px solid #e7e7e7;
+    cursor: pointer;
+`;
+
+const SnsBox = styled.div`
+    padding-top: 40px;
+`;
+const SnsText = styled.div`
+    color: rgb(59, 59, 59);
+    font-size: 14px;
+    font-weight: bold;
+    margin-bottom: 16px;
+    line-height: 14px;
+`;
+const SnsBtnBox = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+const KakaoBtn = styled.button`
+    height: 50px;
+    width: 47%;
+    border-radius: 27.5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 14px;
+    font-weight: bold;
+    background-color: rgb(254, 229, 0);
+    border: 0;
+    cursor: pointer;
+    & img {
+        width: 22px;
+        margin-right: 12px;
+    }
+`;
+const AppleBtn = styled.button`
+    height: 50px;
+    width: 47%;
+    border-radius: 27.5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 14px;
+    font-weight: bold;
+    color: white;
+    background-color: rgb(45, 180, 0);
+    border: 1px solid rgb(218, 218, 218);
+    cursor: pointer;
+    & img {
+        width: 22px;
+        margin-right: 12px;
+    }
+`;
+
+export default Login;
---
+++ src/pages/Signup.tsx
@@ -0,0 +1,259 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import { actionCreators as userActions } from '../redux/modules/user';
+import { emailCheck, idCheck, phoneNumCheck } from '../shared/common';
+
+function Signup() {
+    const dispatch = useDispatch();
+    const [userId, SetUserId] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [passwordCheck, setPasswordCheck] = useState<string>('');
+    const [userName, setUserName] = useState<string>('');
+    const [userEmail, setUserEmail] = useState<string>('');
+    const [phoneNum, setPhoneNum] = useState<string>('');
+
+    const signup = (): void => {
+        // 유효성 검증
+        if (
+            userId === '' ||
+            password === '' ||
+            passwordCheck === '' ||
+            userName === '' ||
+            userEmail === '' ||
+            phoneNum === ''
+        ) {
+            window.alert('회원 정보를 모두 입력하세요.');
+            return;
+        }
+        if (password !== passwordCheck) {
+            window.alert('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
+            return;
+        }
+        if (password.length < 4 || password.length > 16 || passwordCheck.length < 4 || passwordCheck.length > 16) {
+            window.alert('비밀번호는 형식이 맞지 않습니다.');
+            return;
+        }
+        if (!idCheck(userId) || userId.length < 4 || userId.length > 16) {
+            window.alert('아이디 형식이 맞지 않습니다.');
+            return;
+        }
+        if (!emailCheck(userEmail)) {
+            window.alert('이메일 형식이 맞지 않습니다.');
+            return;
+        }
+        if (!phoneNumCheck(phoneNum)) {
+            window.alert('핸드폰 형식이 맞지 않습니다.');
+            return;
+        }
+        dispatch(userActions.SignupDB(userId, password, userName, userEmail, phoneNum));
+    };
+    return (
+        <>
+            <Container>
+                <Header>
+                    <HeaderImg src="" alt="WEROW" />
+                </Header>
+                <MainContainer>
+                    <TitleBox>
+                        <Title>회원 가입</Title>
+                    </TitleBox>
+                    <SignupTitleBox>
+                        <SignupTitle>기본 정보 입력</SignupTitle>
+                    </SignupTitleBox>
+                    <SignupBox>
+                        <table>
+                            <tbody>
+                                {/* <tr>
+                                    <th>아이디</th>
+                                    <td>
+                                        <input
+                                            type="text"
+                                            placeholder="영문, 숫자 4~16자"
+                                            onChange={(e) => {
+                                                SetUserId(e.target.value);
+                                            }}
+                                        />
+                                    </td>
+                                </tr> */}
+                                  <tr>
+                                    <th>이메일</th>
+                                    <td>
+                                        <input
+                                            type="text"
+                                            placeholder="abc@email.com"
+                                            onChange={(e) => {
+                                                setUserEmail(e.target.value);
+                                            }}
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>비밀번호</th>
+                                    <td>
+                                        <input
+                                            type="password"
+                                            placeholder="영문, 숫자, 특수문자 4~16자 이내"
+                                            onChange={(e) => {
+                                                setPassword(e.target.value);
+                                            }}
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>비밀번호 확인</th>
+                                    <td>
+                                        <input
+                                            type="password"
+                                            onChange={(e) => {
+                                                setPasswordCheck(e.target.value);
+                                            }}
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>이름</th>
+                                    <td>
+                                        <input
+                                            type="text"
+                                            placeholder="성과 이름을 모두 입력하세요."
+                                            onChange={(e) => {
+                                                setUserName(e.target.value);
+                                            }}
+                                        />
+                                    </td>
+                                </tr>
+                                {/* <tr>
+                                    <th>이메일</th>
+                                    <td>
+                                        <input
+                                            type="text"
+                                            placeholder="abc@email.com"
+                                            onChange={(e) => {
+                                                setUserEmail(e.target.value);
+                                            }}
+                                        />
+                                    </td>
+                                </tr> */}
+                                <tr>
+                                    <th>휴대폰 번호</th>
+                                    <td>
+                                        <input
+                                            type="text"
+                                            placeholder="ex) 010-0000-0000"
+                                            onChange={(e) => {
+                                                setPhoneNum(e.target.value);
+                                            }}
+                                        />
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </SignupBox>
+                    <div>
+                        <SignupBtn onClick={signup}>통합 회원가입</SignupBtn>
+                    </div>
+                </MainContainer>
+            </Container>
+        </>
+    );
+}
+
+const Container = styled.div`
+    margin: auto;
+    min-width: 290px;
+    max-width: 460px;
+    position: relative;
+`;
+
+const Header = styled.div`
+    height: 42px;
+    line-height: 42px;
+    background: #fff;
+    text-align: center;
+    font-size: 15px;
+    font-weight: bold;
+    color: #0d0d0d;
+    border-bottom: 1px solid #ebebeb;
+`;
+
+const HeaderImg = styled.img`
+    font-size: 18px;
+    height: 16px;
+`;
+const MainContainer = styled.div`
+    margin: auto 40px;
+`;
+
+const TitleBox = styled.div`
+    margin-top: 55px;
+`;
+const Title = styled.div`
+    font-size: 21px;
+    font-weight: bold;
+    text-align: center;
+    color: #0d0d0d;
+    margin-bottom: 5px;
+`;
+const SignupTitleBox = styled.div`
+    margin-top: 30px;
+`;
+const SignupTitle = styled.div`
+    color: #000;
+    font-weight: bold;
+    text-align: center;
+    padding-bottom: 10px;
+    border-bottom: 2px solid #000;
+`;
+
+const SignupBox = styled.div`
+    padding: 15px 20px 20px;
+    & table {
+        border-spacing: 0;
+        border: 0;
+        border-collapse: collapse;
+        width: 100%;
+        height: 100%;
+        & tbody {
+            margin: 0;
+            padding: 0;
+            & th {
+                height: 15px;
+                font-size: 13px;
+                text-align: left;
+                color: #2c2c2c;
+                width: 30%;
+                vertical-align: initial;
+                line-height: 44px;
+            }
+            & td {
+                padding-bottom: 5px;
+                & input {
+                    width: 100%;
+                    height: 44px;
+                    font-size: 13px;
+                    color: #2c2c2c;
+                    margin-bottom: 10px;
+                    border-radius: 2px;
+                    border: 0.7px solid #dadada;
+                    padding: 14px;
+                    box-sizing: border-box;
+                }
+            }
+        }
+    }
+`;
+
+const SignupBtn = styled.button`
+    margin-top: 10px;
+    color: #fff;
+    background-color: #ff6f61;
+    width: 100%;
+    height: 45px;
+    border-radius: 2px;
+    font-size: 14px;
+    border: 1px solid #e7e7e7;
+    cursor: pointer;
+`;
+
+export default Signup;
