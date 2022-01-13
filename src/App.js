a06dea07603e6fdea1890f41c69b0d0e5033d8d8
youngho <youngho7482@gmail.com>
2022-01-14 01:18:16 +0900

    Revert "Initialize project using Create React App"
    This reverts commit 71b8710e6dbbaf967945e314b95c9b6aa48c21e0.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import Problem from './components/Problem';
-import Todos from './components/Todos';
-import TodoProblem from './components/TodoProblem';
-import { Link, Route, Switch } from 'react-router-dom';
-import styled, {createGlobalStyle} from 'styled-components';
-import { Fragment } from 'react';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import WritePage from './pages/WritePage';
-import PostListPage from './pages/PostListPage';
-import PostPage from './pages/PostPage';
 
 function App() {
   return (
-    <div>
-      <Fragment>
-          {/* <GlobalStyle/> */}
-      {/* <Problem title={'검증수'}/> */}
-       {/* <h1><Link to="/Todos">문제풀러가기</Link></h1> */}
-      <Switch>
-        {/* <Route exact path ='/Todos' component = {<Todos/>} />
-        <Route exact path ='/TodoProblem' component ={<TodoProblem/>} />
-        <Route exact path ='/problem' component ={<Problem />} /> */}
-        <Route exact path ='/@:username' component={PostListPage}/>
-        <Route exact path ='/login' component ={LoginPage} />
-        <Route exact path ='/register' component ={RegisterPage} />
-        <Route exact path ='/write' component={WritePage} />
-        <Route exact path ='/' component={PostListPage}/>
-        <Route exact path ='/@:username/:postId' component={PostPage} />
-      </Switch>
-      
-      {/* <Todos/> */}
-      </Fragment>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> parent of bf7cfc2... CodingProblem FrontPage First Developing
     </div>
   );
 }
 
-const GlobalStyle = createGlobalStyle`
-  body{
-    background-color: #c5cfda;
-  }
-`
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router } from 'react-router-dom';
-import rootReducer, { rootSaga } from './modules';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import { createStore,applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import createSagaMiddleware from 'redux-saga';
-import { tempSetUser,check } from './modules/user';
+import axios from 'axios';
+import { getCookie } from './shared/Cookie';
+import App from './shared/App';
+import store from './redux/configureStore';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals';
 
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+axios.interceptors.request.use((config) => {
+    const token = getCookie('is_login');
+    config.headers.Authorization = `Bearer ${token}`;
 
-function loadUser() {
-  try{
-    const user = localStorage.getItem('user');
-    if(!user) return; //로그인 상태가 아니라면 아무것도 안함
-    store.dispatch(tempSetUser(JSON.parse(user)));
-    store.dispatch(check());
-  }catch(e) {
-    console.log('localStorage is not working');
-  }
-}
-
-sagaMiddleware.run(rootSaga);
-loadUser();
+    return config;
+});
 
 ReactDOM.render(
-  <Provider store ={store}>
-    <Router>
-    <App />
-    </Router>
-  </Provider>, 
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
